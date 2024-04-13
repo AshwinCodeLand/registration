62fc34fc373e1fa737cd020bb0932b6be4106268
AshwinCodeLand <dev.ashwinalexander@gmail.com>
2024-04-13 14:22:31 +0530

    latest

--- Backend/db.sql
+++ Backend/db.sql
@@ -1,8 +1,16 @@
+create database Coalesce;
+use Coalesce;
 Create Table users (
 userid int NOT NULL AUTO_INCREMENT,
 username varchar(500),
 email varchar(500),
-verify boolean,
 password varchar(50);
+verify boolean,
 PRIMARY KEY (userid)
 );
+
+
+
+
+
+
